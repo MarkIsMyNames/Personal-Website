b39b0ec890b18af49cdf68c83b22d09b2b430321
Bob <mrakemail@proton.me>
2026-04-03 23:45:40 +0100

    Fix some lighthouse remove unnessessary vite things

--- vite.config.ts
+++ vite.config.ts
@@ -17,17 +17,6 @@ export default defineConfig({
   ],
   build: {
     outDir: 'build',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          router: ['react-router-dom'],
-          i18n: ['i18next', 'react-i18next'],
-          icons: ['react-icons'],
-          helmet: ['react-helmet-async'],
-          styling: ['styled-components'],
-        },
-      },
-    },
   },
   test: {
     globals: true,
